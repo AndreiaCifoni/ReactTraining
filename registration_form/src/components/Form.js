a85c1761497d47b2cf527f42544398c0d60aa27a
Andreia Cifoni <andreiacifoni@gmail.com>
2022-06-17 15:25:14 -0400

    inputs done

--- registration_form/src/components/Form.js
+++ registration_form/src/components/Form.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [term, setTerm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="email" placeholder="Email" />
+      <form onSubmit={onSubmitHandler}>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={term.firstName}
+            onChange={(e) => setTerm({ ...term, firstName: e.target.value })}
+          />
+          {onSubmitHandler && term.firstName !== "" ? null : (
+            <span>This field is mandatory</span>
+          )}
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={term.lastName}
+            onChange={(e) => setTerm({ ...term, lastName: e.target.value })}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={term.email}
+            onChange={(e) => setTerm({ ...term, email: e.target.value })}
+          />
+        </div>
         <button type="submit">Register</button>
       </form>
     </div>
