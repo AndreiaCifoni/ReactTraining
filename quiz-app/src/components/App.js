2c2645ad6867a4b3c792c24f91d63ff833ff034a
Andreia Cifoni <andreiacifoni@gmail.com>
2022-06-18 19:08:55 -0400

    quiz array refactored

--- quiz-app/src/components/App.js
+++ quiz-app/src/components/App.js
@@ -4,98 +4,108 @@ import Quiz from "./Quiz";
 const quiz = [
   {
     question: "What was the first pokemon that Ash received?",
-    option1: {
-      key: "Charmander",
-      answer: false,
-    },
-    option2: {
-      key: "Bulbasaur",
-      answer: false,
-    },
-    option3: {
-      key: "Pikachu",
-      answer: true,
-    },
-    option4: {
-      key: "Squirtle",
-      answer: false,
-    },
+    options: [
+      {
+        key: "Charmander",
+        answer: false,
+      },
+      {
+        key: "Bulbasaur",
+        answer: false,
+      },
+      {
+        key: "Pikachu",
+        answer: true,
+      },
+      {
+        key: "Squirtle",
+        answer: false,
+      },
+    ],
   },
   {
     question: "Who was Ash's firts rival?",
-    option1: {
-      key: "Gary",
-      answer: true,
-    },
-    option2: {
-      key: "Brock",
-      answer: false,
-    },
-    option3: {
-      key: "Trevor",
-      answer: false,
-    },
-    option4: {
-      key: "Serena",
-      answer: false,
-    },
+    options: [
+      {
+        key: "Gary",
+        answer: true,
+      },
+      {
+        key: "Brock",
+        answer: false,
+      },
+      {
+        key: "Trevor",
+        answer: false,
+      },
+      {
+        key: "Serena",
+        answer: false,
+      },
+    ],
   },
   {
     question: "What type of pokemon is Psyduck?",
-    option1: {
-      key: "Fire",
-      answer: false,
-    },
-    option2: {
-      key: "Water",
-      answer: false,
-    },
-    option3: {
-      key: "Earth",
-      answer: false,
-    },
-    option4: {
-      key: "Psychic",
-      answer: true,
-    },
+    options: [
+      {
+        key: "Fire",
+        answer: false,
+      },
+      {
+        key: "Water",
+        answer: false,
+      },
+      {
+        key: "Earth",
+        answer: false,
+      },
+      {
+        key: "Psychic",
+        answer: true,
+      },
+    ],
   },
   {
     question: "What is the evolution of Kadabra?",
-    option1: {
-      key: "Geodude",
-      answer: false,
-    },
-    option2: {
-      key: "Haunter",
-      answer: false,
-    },
-    option3: {
-      key: "Alakazan",
-      answer: true,
-    },
-    option4: {
-      key: "Hypno",
-      answer: false,
-    },
+    options: [
+      {
+        key: "Geodude",
+        answer: false,
+      },
+      {
+        key: "Haunter",
+        answer: false,
+      },
+      {
+        key: "Alakazan",
+        answer: true,
+      },
+      {
+        key: "Hypno",
+        answer: false,
+      },
+    ],
   },
   {
     question: "Where does the pokemon Chansey works?",
-    option1: {
-      key: "Hospital",
-      answer: true,
-    },
-    option2: {
-      key: "Aquarium",
-      answer: false,
-    },
-    option3: {
-      key: "Gymnasium",
-      answer: false,
-    },
-    option4: {
-      key: "Restaurant",
-      answer: false,
-    },
+    options: [
+      {
+        key: "Hospital",
+        answer: true,
+      },
+      {
+        key: "Aquarium",
+        answer: false,
+      },
+      {
+        key: "Gymnasium",
+        answer: false,
+      },
+      {
+        key: "Restaurant",
+        answer: false,
+      },
+    ],
   },
 ];
 
