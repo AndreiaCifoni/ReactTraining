0dfd3512365f0a3d88daeb21fa2228cd9e11374d
Andreia Cifoni <andreiacifoni@gmail.com>
2022-06-20 15:11:20 -0400

    MenuItem criated, nav link logic created

---
+++ navbar/src/components/MenuItem.js
@@ -0,0 +1,24 @@
+const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-link",
+  },
+  {
+    title: "Services",
+    url: "#",
+    cName: "nav-link",
+  },
+  {
+    title: "Products",
+    url: "#",
+    cName: "nav-link",
+  },
+  {
+    title: "Constact us",
+    url: "#",
+    cName: "nav-link",
+  },
+];
+
+export default MenuItems;
--- navbar/src/components/Navbar.js
+++ navbar/src/components/Navbar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./navbar.css";
+import MenuItems from "./MenuItem";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="nav-container">
+      <h1 className="nav-title">React</h1>
+      <div className="nav-icon"></div>
+      <ul>
+        {MenuItems.map((item) => {
+          return (
+            <li>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <button>Sign up</button>
+    </nav>
+  );
 };
 
 export default Navbar;
