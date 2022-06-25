f14cd4edfbd490b0ccea4285d3c03b40ee7caa23
Andreia Cifoni <andreiacifoni@gmail.com>
2022-06-24 22:13:53 -0400

    added add todo functionality

--- todo-list/src/components/TodoAdd.js
+++ todo-list/src/components/TodoAdd.js
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
-import { PlusIcon } from "@heroicons/react/solid";
+import { PlusIcon, CheckCircleIcon, TrashIcon } from "@heroicons/react/solid";
+import { v4 as uuidv4 } from "uuid";
 import ".././layout/layout.css";
 
 const TodoAdd = () => {
-  const [todo, setTodo] = useState("");
+  const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const onChangeAddTodo = (e) => {
-    setTodo(e.target.value);
+  const onChangeInput = (e) => {
+    setTodoInput(e.target.value);
+  };
+
+  const onClickAddTodo = () => {
+    setTodos([...todos, { task: todoInput, id: uuidv4() }]);
   };
 
   return (
     <div>
-      <input type="text" value={todo} onChange={onChangeAddTodo} />
-      <button>
+      <input type="text" value={todoInput} onChange={onChangeInput} />
+      <button onClick={onClickAddTodo}>
         <PlusIcon className="icon" />
       </button>
+      <ul>
+        {todos.map((todo) => (
+          <li>
+            {todo.task}
+            <CheckCircleIcon className="icon" />
+            <TrashIcon className="icon" />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default TodoAdd;
+
+// uuidv4()
---
+++ todo-list/src/components/TodoList.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { CheckCircleIcon, TrashIcon } from "@heroicons/react/solid";
+// import ".././layout/layout.css";
+
+// const TodoList = ({ todo }) => {
+//   return (
+//     <div>
+//       <ul>
+//         {todo.map((task) => (
+//           <li>
+//             {task}
+//             <CheckCircleIcon />
+//             <TrashIcon />
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default TodoList;
